bb2ca2107bfcff5a050075f1e4b38e858ca70698
ELLISON <ST04229@atr-aircraft.com>
2021-06-18 14:53:12 +0200

    remove obj and bin from tracked files

--- Program.cs
+++ Program.cs
@@ -17,14 +17,17 @@ namespace CopaAmericaRecorder
         {
             Console.WriteLine("Hello");
             Process.Start("firefox.exe", "https://www.canalplus.com/live/?channel=451");
-            //Process[] p = Process.GetProcessesByName("microsoft-edge");
             Thread.Sleep(8000);
             //AuthenticateToMyCanal();
             //Process.Start("microsoft-edge:https://www.canalplus.com/live/?channel=451");
-            //OpenRecorderExtensionAndLaunch();
-            Process.Start(@"C:\\Program Files (x86)\Bandicam\bdcam.exe");
-            Thread.Sleep(15000);
-            SendKeys.SendWait("{F12}");
+            //Process.Start(@"C:\\Program Files\obs-studio\bin\64bit\obs64.exe");
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.WorkingDirectory = @"C:\Program Files\obs-studio\bin\64bit"; // like cd path command
+            startInfo.FileName = "obs64.exe";
+            Process.Start(startInfo);
+            Thread.Sleep(10000);
+            SendKeys.SendWait("$");
+            Thread.Sleep(3000);
             SendKeys.SendWait("%{TAB}");
             Thread.Sleep(2000);
             SendKeys.SendWait("{RIGHT}");
